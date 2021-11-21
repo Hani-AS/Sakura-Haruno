cf3b6d2a2f1293d1b671d9dd088155ce502ac128
Hani al Sabti <h.sabti87@gmail.com>
2021-11-21 13:04:18 +0100

    created vue app

---
+++ app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-container">
+    <NavBar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style lang="scss">
+html,
+body {
+  font-family: "Poppins", sans-serif;
+  color: #2c2851;
+}
+.main-container {
+  height: 100vh;
+  width: 100%;
+  background: rgb(255, 244, 245);
+  background: linear-gradient(
+    180deg,
+    rgba(255, 244, 245, 1) 0%,
+    rgba(251, 251, 251, 1) 100%
+  );
+}
+</style>
